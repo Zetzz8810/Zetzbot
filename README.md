7703879fd49335b424e3fae7584a7327147e8144
Rahmad?? <163720474+Zetzz8810@users.noreply.github.com>
2024-03-17 15:49:15 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+<p align="center">
+    <img src="https://telegra.ph/file/0aa23c7f060e0919e52cf.jpg" width="100%" style="margin-left: auto;margin-right: auto;display: block;">
+    </p>
+    <h1 align="center">Vin - MD</h1>
+
+## Baca Sebelum Pakai!!
+- ubah informasi di config.js
+- support panel,termux & replit
+- Pastiakan Kamu Sudah Subrek Chanel gw
+- Harap Manuruh Credit Jika Ingin Recode Jika Tidak Di Pastikan Tidak Ada Update Lagi Ke Versi Selanjutnya
+
+### Sedikit Tentang Bot
+- ✔️ | **Simple** 
+- ✔️ | **No Button** 
+- ✔️ | **Multi Device** 
+- ✔️ | **Work All Fitur**
+- ✔️ | **No Apikey**
+- ---------
+### Fitur Yang Dimiliki
+- ✔️ | Tools 
+- ✔️ | Remini
+- ✔️ | Quotes
+- ✔️ | Confess
+- ✔️ | Chat Gpt
+- ✔️ | Werewolf Games
+- ✔️ | And Others
+- ✔️ | Bug Warcall
+- ✔️ | Create Panel Pterodactyl
+- ✔️ | Jadi Bot Pairing
+- ✔️ | Full Fitur AI
+- ✔️ | Setmenu (v1,v2,v3,v4)
+- ✔️ | Dan Masih Banyak Lagi
+- ---------
+
+
+##
+[![Group Chat](https://img.shields.io/badge/Owner%20BOT-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://chat.whatsapp.com/BkdLVSrEZa4EviQ7COGnpT) 
+[![Bot](https://img.shields.io/badge/Bot%20Whatsapp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://wa.me/6283150418588)
+
+
+𝐀𝐮𝐭𝐡𝐨𝐫 : VnShoop|@vinwert
+Whatsapp : 083150418588
+
---
+++ config.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+
+global.namabot = "ZetzBotMD"
+global.namaowner = "ZKing"
+global.packname = "©ZetzzBot"
+global.creator = "Z-Team"
+global.author = "Ardy"
+global.wm = "Zetz"
+global.myweb = "https://zetzz.my.id"
+global.footer_text = "©Zetz "
+global.owner = ['6281239621820']
+global.doc = "ᴍɪʟᴋɪᴛᴀ ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ"
+//===============================
+global.apikey = '8c3a5b302815a138d88148fa0c5916c0595bba50' //ambil dari web ini https://otpweb.com
+//==================================
+global.qris = "https://-"
+global.dana = "081239621820"
+global.gopay = "081239621820"
+global.rek = "7605-0101-8289-536"
+//================================
+global.fake = "https://telegra.ph/file/55df8393d102d69ead186.jpg"
+global.gifin = "https://telegra.ph/file/00224f1c0b07a10555f09.mp4"
+global.news = "𝙕𝙚𝙩𝙯𝘽𝙤𝙩𝙈𝘿|©ᴢᴇᴛᴢᴀᴜᴛᴏʀᴇsᴘᴏɴᴛʙᴏᴛ"
+global.syt = "https://www.youtube.com/@imzetzz"
+global.sgc = "https://"
+global.email = "ɴᴜʀʀ67813@gmail.com"
+global.sig = "https://Instagram.com/@-"
+//==========================
+global.pairingNumber = "6281239621820" 
+global.prefa = ['.']
+global.tekspushcontact = "Izin Push"
+global.typemenu = "v1"
+global.grup_only = true 
+global.antispam = true
+global.auto_record = true
+global.anticall = true
+global.gcount = {
+     prem: 1000,
+     user: 20,
+}
+global.mess = {
+    done: '_Success_',
+    wait: '_Sedang Di Proses_',
+    admin: '_Fitur Khusus Admin Group_',
+    botNotAdmin: '_Jadikan Bot Sebagai Admin Terlebih Dahulu_',
+    owner: '_Fitur Khusus Owner Bot_',
+    group: '_Fitur Khusus Dalam Group_',
+}
+
